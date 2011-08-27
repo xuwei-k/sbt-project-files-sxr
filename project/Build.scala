b7513b8a7ad273bdbdc17ff43295ea5fae8bf9e3
xuwei_k <6b656e6a69@gmail.com>
2011-08-27 21:20:51 +0900

    add sbt

--- project/Build.scala
+++ project/Build.scala
@@ -9,9 +9,9 @@ case class ProjectDefFile(
 ){
 
   def sourceURL(sourceName:String) =
-    Seq("https://raw.github.com",user,projectName,tree,sourceName).mkString("/")
+    Seq("https://raw.github.com",user,projectName,tree,"project",sourceName).mkString("/")
 
-  lazy val rawSourceList:Seq[(String,String)] = sourceList.map{ s =>
+  def rawSourceList:Seq[(String,String)] = sourceList.map{ s =>
     s -> scala.io.Source.fromURL(sourceURL(s)).mkString
   }
 
@@ -20,9 +20,15 @@ case class ProjectDefFile(
 object ProjectDefFiles{
   
   lazy val list = Seq(
-    ProjectDefFile("scalaz","scalaz",Seq("project/Boilerplate.scala","project/ScalazBuild.scala"))
-    //ProjectDefFile("","",""),
-
+    ProjectDefFile("scalaz","scalaz",Seq("Boilerplate.scala","ScalazBuild.scala"))
+   ,ProjectDefFile(
+      "harrah",
+      "xsbt",
+      Seq("Sbt.scala","Util.scala","Sbt.scala","Status.scala"
+         ,"Sxr.scala","Proguard.scala","Release.scala","Transform.scala"
+      ),
+      "0.10"
+    )
   )
 }
 
@@ -33,7 +39,8 @@ object ScalaKaigi extends Build{
     file("."),
     settings = Defaults.defaultSettings ++ Seq(
       libraryDependencies ++= Seq(
-        "org.scala-tools.sbt" %% "sbt" % "0.10.0"
+        "org.scala-tools.sbt" %% "sbt" % "0.10.1"
+       ,"net.databinder" %% "dispatch-http" % "0.8.3"
       ),
       addCompilerPlugin("org.scala-tools.sxr" %% "sxr" % "0.2.7")
      ,(sourceGenerators in Compile) <+= (sourceManaged in Compile) map{
